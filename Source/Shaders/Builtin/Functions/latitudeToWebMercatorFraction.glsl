3c77735c9a27b71cc7b0b501d89e136455476836
chabou-san <d.chabou@gmail.com>
2015-01-05 18:28:44 +0100

    Revert the commit #917263937a3b8a668d4d51a31398a313be06bb47 wich was deleted by error

--- Source/Shaders/Builtin/Functions/latitudeToWebMercatorFraction.glsl
+++ Source/Shaders/Builtin/Functions/latitudeToWebMercatorFraction.glsl
@@ -12,18 +12,18 @@
  * @returns {float} The fraction of the rectangle at which the latitude occurs.  If the latitude is the southern
  *          boundary of the rectangle, the return value will be zero.  If it is the northern boundary, the return
  *          value will be 1.0.  Latitudes in between are mapped according to the Web Mercator projection.
- */ 
+ */
 float czm_latitudeToWebMercatorFraction(float latitude, float southMercatorYLow, float southMercatorYHigh, float oneOverMercatorHeight)
 {
-    float sinLatitude = sin(latitude);
+    float sinLatitude = czm_cosineAndSine(latitude).y;
     float mercatorY = 0.5 * log((1.0 + sinLatitude) / (1.0 - sinLatitude));
-    
+
     // mercatorY - southMercatorY in simulated double precision.
     float t1 = 0.0 - southMercatorYLow;
     float e = t1 - 0.0;
     float t2 = ((-southMercatorYLow - e) + (0.0 - (t1 - e))) + mercatorY - southMercatorYHigh;
     float highDifference = t1 + t2;
     float lowDifference = t2 - (highDifference - t1);
-    
+
     return highDifference * oneOverMercatorHeight + lowDifference * oneOverMercatorHeight;
 }
